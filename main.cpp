207e295e2bebda2753ad760e64ca9fb8c0307a96
francesco <fr.sanfilippo@gmail.com>
2015-10-26 16:32:07 +0000

    Added matr_t and defined X,P

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <Eigen/Dense>
+#include <complex>
+#include <iostream>
+#include <vector>
+
+using namespace Eigen;
+using namespace std;
 
 double T=10;
 double dt=0.1;
 
-int N=4;
+//define the type
+const int N=4;
+const int glb_N=9;
+typedef Matrix<complex<double>,N,N> matr_t;
+
+vector<matr_t> X(glb_N),P(glb_N);
 
 //
 void generate_random_matrices()
@@ -20,11 +31,15 @@ void measure_observables()
 
 int main()
 {
+  cout<<X[0]<<endl;
+  
   //generate random variables
   generate_random_matrices();
   
   //compute number of steps needed to integrate
   int nt=T/dt;
+  
+  //first half step
   
   //integrate equation of motion
   for(int it=0;it<nt;it++)
