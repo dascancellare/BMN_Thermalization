4fff099424d1155971ef9e08ddc8a6e071979d76
francesco <fr.sanfilippo@gmail.com>
2015-10-28 18:51:56 +0000

    fixed a coefficeient 3 not understood

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Eigen/Dense>
 #include <complex>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -14,7 +15,8 @@ typedef Matrix<complex<double>,N,N> matr_t;
 
 //length of trajectory
 double T=10;
-double dt=0.1;
+double t=0;
+double dt=0.01;
 
 //parameters of the initial conditions
 const double h=0.001;
@@ -40,7 +42,9 @@ matr_t comm(matr_t a,matr_t b)
 complex<double> get_gauss()
 {return gauss(gen)+I*gauss(gen);}
 
+ofstream energy_file("energy");
 //define the matrices of su(2) representation of dimension j
+/*
 void generate_L (int j)
 {
   int dimrep=2j+1;
@@ -59,7 +63,7 @@ void generate_L (int j)
   for(int indn=0,ind<dimrep,ind++)
     J3(ind,ind)=j-ind;
 }
-
+*/
 //put everything to random and then overwrite with L
 void generate_matrices()
 {
@@ -84,6 +88,40 @@ void generate_matrices()
   P[0](N-1,N-1)=v;
 }
 
+double trace_square(matr_t M)
+{return (M*M).trace().real();}
+
+//compute hamiltonian
+double hamiltonian()
+{
+  //momenta
+  double K=0;
+  for(int i=0;i<glb_N;i++) K+=trace_square(P[i]);
+  K/=2;
+  
+  //potential X
+  double V=0;
+  for(int i=0;i<nX;i++) V+=trace_square(X[i]+I*comm(X[(i+1)%nX],X[(i+2)%nX]));
+  
+  //potential Y
+  for(int a=nX;a<glb_N;a++) V+=trace_square(X[a])/4;
+  
+  //potential X-Y
+  for(int i=0;i<nX;i++)
+    for(int a=nX;a<glb_N;a++)
+      V+=-trace_square(comm(X[i],X[a]));
+  
+  //potential Y-Y
+  for(int a=nX;a<glb_N;a++)
+    for(int b=nX;b<glb_N;b++)
+      V+=-trace_square(comm(X[a],X[b]))/2;
+  
+  //add final 1/2
+  V/=2;
+  
+  return K+V;
+}
+
 //update the positions on the base of momenta
 void update_positions(double dt)
 {for(int i=0;i<glb_N;i++) X[i]+=P[i]*dt;}
@@ -92,13 +130,33 @@ void update_momenta(double dt)
 {
   // calcola la forza
   vector<matr_t> F(glb_N);
+  for(auto &Fi : F) Fi.setZero();
   
   //primo pezzo diverso per X ed Y
-  for(int i=0;i<nX;i++) F[i]=-X[i]-6.0*I*comm(X[(i+1)%nX],X[(i+2)%nX]);
-  for(int i=nX;i<glb_N;i++) F[i]=-0.25*X[i];
+  for(int i=0;i<nX;i++) F[i]=-X[i]-3.0*I*comm(X[(i+1)%nX],X[(i+2)%nX]);
+  for(int a=nX;a<glb_N;a++) F[a]=-0.25*X[a];
   
   //secondo pezzo uguale per tutti
   for(int i=0;i<glb_N;i++) for(int a=0;a<glb_N;a++) F[i]+=comm(comm(X[a],X[i]),X[a]);
+  
+  //#define DEBUG
+  
+#ifdef DEBUG
+  int icheck=1;
+  double e_bef=hamiltonian();
+
+  double eps=1e-6;
+  X[icheck](1,0)+=eps/2;
+  X[icheck](0,1)+=eps/2;
+  
+  double e_aft=hamiltonian();
+  double f_num=-(e_aft-e_bef)/eps;
+  double f_exa=(F[icheck](1,0)+F[icheck](0,1)).real()/2.0;
+  cout<<"Exa: "<<f_exa<<", num: "<<f_num<<endl;
+  
+  X[icheck](1,0)-=eps;
+  X[icheck](0,1)-=eps;
+#endif
   
   ////
   
@@ -111,39 +169,6 @@ void integration_step()
 {
   update_positions(dt);
   update_momenta(dt);
-}
-
-double trace_square(matr_t M)
-{return (M*M).trace().real();}
-
-//compute hamiltonian
-double hamiltonian()
-{
-  double H=0;
-  
-  //momenta
-  for(int i=0;i<glb_N;i++) H+=(P[0]*P[0]).trace().real();
-  
-  //potential X
-  for(int i=0;i<nX;i++) H+=trace_square(X[i]+I*comm(X[(i+1)%nX],X[(i+2)%nX]));
-  
-  //potential Y
-  for(int a=nX;a<glb_N;a++) H+=trace_square(X[a]*X[a])/4;
-  
-  //potential Y-Y
-  for(int i=0;i<nX;i++)
-    for(int a=nX;a<glb_N;a++)
-      H+=-trace_square(comm(X[i],X[a]));
-  
-  //potential Y-Y
-  for(int a=nX;a<glb_N;a++)
-    for(int b=nX;b<glb_N;b++)
-      H+=-trace_square(comm(X[a],X[b]))/2;
-  
-  //add final 1/2
-  H/=2;
-  
-  return H;
 }
 
 matr_t constraint()
@@ -159,13 +184,14 @@ matr_t constraint()
 //
 void measure_observables()
 {
-  cout<<X[0]<<endl;
-  cout<<"Hamiltonian: "<<endl<<hamiltonian()<<endl<<endl;
-  cout<<"Constraint: "<<endl<<constraint()<<endl<<endl;
+  energy_file<<t<<" "<<hamiltonian()<<endl;
+  //cout<<"Constraint: "<<endl<<constraint()<<endl<<endl;
 }
 
 int main()
 {
+  energy_file.precision(16);
+  
   //generate random matrices+L
   generate_matrices();
   
@@ -174,17 +200,21 @@ int main()
   cout<<"Number of integration steps: "<<nt<<" to integrate "<<T<<" in steps of "<<dt<<endl;
   
   //first half step: p(dt/2)= p(0)+ F(0)*dt/2
-  //update_momenta(dt/2);
+  update_momenta(dt/2);
   
   //integrate equation of motion using leapfrog
   for(int it=0;it<nt;it++)
     {
       cout<<"Integration step "<<it+1<<"/"<<nt<<endl;
       
+      update_positions(dt);
+      update_momenta(dt/2);
+      
       measure_observables();
       
-      integration_step();
+      update_momenta(dt/2);
       
+      t+=dt;
     }
   
   return 0;
